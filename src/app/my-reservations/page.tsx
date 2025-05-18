af7798c4a33e56e0c45e50245b0c4885d7fdfbfe
pathummadhusanka <commits@pathummadhusanka.com>
2025-05-18 18:12:02 +0530

    feat(reservations): create my-reservations page

--- src/app/my-reservations/page.tsx
+++ src/app/my-reservations/page.tsx
@@ -1,12 +1,45 @@
-import SidebarLayout from '@/layouts/Sidebar/Layout';
-import NotFound from '../not-found';
+'use client';
 
-function page() {
+import SidebarLayout from '@/layouts/Sidebar/Layout';
+import PageHeader from '@/components/custom/PageHeader';
+import Loading from '@/components/custom/Loading';
+import { useState, useEffect } from 'react';
+import { UnifiedReservationRow } from './reservation';
+import { reservationColumns } from './reservationColumns';
+import { DataTable } from './data-table';
+import { getUnifiedReservations } from './getUnifiedReservations';
+
+function Page() {
+	const [data, setData] = useState<UnifiedReservationRow[] | null>(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const unifiedData = await getUnifiedReservations();
+				setData(unifiedData);
+			} catch (err) {
+				console.error('Failed to fetch reservations', err);
+			} finally {
+				setLoading(false);
+			}
+		}
+
+		fetchData();
+	}, []);
+
 	return (
 		<SidebarLayout>
-			<NotFound />
+			<PageHeader title='Reservation History' />
+			<div className='container mx-auto'>
+				{loading || data === null ? (
+					<Loading reason='Loading Reservations' pageView />
+				) : (
+					<DataTable columns={reservationColumns} data={data} />
+				)}
+			</div>
 		</SidebarLayout>
 	);
 }
 
-export default page;
+export default Page;
