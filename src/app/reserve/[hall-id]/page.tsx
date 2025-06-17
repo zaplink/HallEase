7ca1114e5eaeea13bfa62b341e780c4390e3c096
pathummadhusanka <pathum23200@gmail.com>
2025-06-17 13:24:55 +0530

    feat: add option to change requesting hall

--- src/app/reserve/[hall-id]/page.tsx
+++ src/app/reserve/[hall-id]/page.tsx
@@ -13,11 +13,15 @@ import { useState } from 'react';
 import ReserveEventForm from '../forms/event/ReserveEventForm';
 import ReserveLectureForm from '../forms/lecture/ReserveLectureForm';
 import { useParams } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { useRouter } from 'next/navigation';
 
 export default function ReservePage() {
 	const [purpose, setPurpose] = useState<string | undefined>(undefined);
 	const params = useParams();
 	const hallId = params['hall-id'];
+	const router = useRouter();
 
 	return (
 		<SidebarLayout>
@@ -25,15 +29,27 @@ export default function ReservePage() {
 				title={`Reserve ${hallId}`}
 				descriptions={['Let’s get everything set up for you.']}
 				extra={
-					<Select onValueChange={setPurpose}>
-						<SelectTrigger className='w-[180px]'>
-							<SelectValue placeholder='Purpose' />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectItem value='event'>Event</SelectItem>
-							<SelectItem value='lecture'>Lecture</SelectItem>
-						</SelectContent>
-					</Select>
+					<div className='flex flex-row gap-2 items-center'>
+						<Button
+							variant={'link'}
+							onClick={() => router.push(`/reserve`)}
+						>
+							Reserve Another Hall
+						</Button>
+						<Separator
+							orientation='vertical'
+							className='h-6 mx-4'
+						/>
+						<Select onValueChange={setPurpose}>
+							<SelectTrigger className='w-[180px]'>
+								<SelectValue placeholder='Purpose' />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectItem value='event'>Event</SelectItem>
+								<SelectItem value='lecture'>Lecture</SelectItem>
+							</SelectContent>
+						</Select>
+					</div>
 				}
 			/>
 
