5cc32a2066c1464d7f72c9b80251a25b192589d6
UdasriHasindu <usrimart@gmail.com>
2025-07-10 10:35:43 +0530

    feat: add new sections to register form

--- src/lib/SignupActions.ts
+++ src/lib/SignupActions.ts
@@ -9,18 +9,27 @@ export async function signup(formData: FormData) {
 	const data = {
 		email: formData.get('email') as string,
 		password: formData.get('password') as string,
-		phone: formData.get('phone') as string,
+		position: formData.get('position') as string,
 		username: formData.get('username') as string,
+		pro_pic: formData.get('pro_pic') as string,
+		role: formData.get('role') as string,
 	};
 
-	if (!data.username || !data.email || !data.phone || !data.password) {
+	if (
+		!data.username ||
+		!data.email ||
+		!data.position ||
+		!data.password ||
+		!data.pro_pic ||
+		!data.role
+	) {
 		redirect(
 			'/access-control/register/error?message=' +
 				encodeURIComponent('Missing fields in the form.')
 		);
 	}
 
-	// 1. Create user in auth.users
+	// Create user in auth.users
 	const { data: signupData, error } = await supabase.auth.signUp({
 		email: data.email,
 		password: data.password,
@@ -44,12 +53,14 @@ export async function signup(formData: FormData) {
 		);
 	}
 
-	// Optionally insert into custom users table
+	// Insert into profiles table to user details
 	await supabase.from('profiles').insert({
 		id: signupData.user?.id,
-		email: data.email,
-		position: data.phone,
 		full_name: data.username,
+		role: data.role,
+		email: data.email,
+		position: data.position,
+		pro_pic: data.pro_pic,
 	});
 
 	return { success: true, message: 'Invitation sent to user email.' };
