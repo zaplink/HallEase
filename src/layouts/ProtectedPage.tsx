6fd09462c5c62b71048ecbc0bf67ddd5a264bfb7
pathummadhusanka <pathum23200@gmail.com>
2025-07-10 13:13:19 +0530

    fix: unnecessary loading

--- src/layouts/ProtectedPage.tsx
+++ src/layouts/ProtectedPage.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { createClient } from '@/lib/supabaseClient';
 import { User } from '@supabase/supabase-js';
-import Loading from '@/components/custom/Loading';
 
 export default function ProtectedPage({
 	children,
@@ -11,31 +10,21 @@ export default function ProtectedPage({
 	children: React.ReactNode;
 }) {
 	const router = useRouter();
-	const [user, setUser] = useState<User | null | 'loading'>('loading'); // Prevents flickering
+	const [isAuthChecked, setIsAuthChecked] = useState(false);
 
 	useEffect(() => {
 		const supabase = createClient();
 
 		supabase.auth.getUser().then(({ data, error }) => {
-			// Redirect to login if not authenticated
 			if (error || !data?.user) {
 				router.replace('/login');
-			} else {
-				setUser(data.user);
 			}
+			setIsAuthChecked(true);
 		});
 	}, [router]);
 
-	if (user === 'loading')
-		return (
-			// <div className='flex justify-center items-center h-screen'>
-			// 	<p className='text-blue-950 text-lg'>
-			// 		Checking authentication...
-			// 	</p>
-			// </div>
+	// Don't show anything while checking auth
+	if (!isAuthChecked) return null;
 
-			<Loading text='Checking Authentication' pageView={true} />
-		);
-
-	return <>{children}</>;
+	return children;
 }
