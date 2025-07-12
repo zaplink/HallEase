9b63c408a910e376c21e2ac4a798b7cadee1c14d
pathummadhusanka <pathum23200@gmail.com>
2025-07-12 18:15:02 +0530

    fix: /metrics-cards.tsx

--- src/components/ui/metrics-cards.tsx
+++ src/components/ui/metrics-cards.tsx
@@ -1,12 +1,6 @@
 'use client';
 
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import {
 	CalendarDays,
