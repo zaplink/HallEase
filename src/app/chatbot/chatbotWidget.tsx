5e5e9d682870a11fd19d7e3e18cf81922e9e5539
UdasriHasindu <usrimart@gmail.com>
2025-07-13 20:27:45 +0530

    feat: customize chatbotWidget

--- src/app/chatbot/chatbotWidget.tsx
+++ src/app/chatbot/chatbotWidget.tsx
@@ -1,9 +1,7 @@
-// components/ChatbotWidget.tsx
-
 'use client';
 
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { Library } from 'lucide-react'; // or use your own icon
 
 export default function ChatbotWidget() {
 	const router = useRouter();
@@ -13,12 +11,20 @@ export default function ChatbotWidget() {
 	};
 
 	return (
-		<button
-			onClick={handleClick}
-			aria-label='Open Chatbot'
-			className='fixed bottom-6 right-6 z-50 bg-blue-600 hover:bg-blue-700 text-white p-4 rounded-full shadow-lg transition-all duration-300 ease-in-out'
-		>
-			<Library className='w-6 h-6' />
-		</button>
+		<div className='fixed bottom-6 right-6 z-50 group'>
+			<button
+				onClick={handleClick}
+				aria-label='Open Chatbot'
+				className='p-2 rounded-full bg-blue-700 shadow-lg hover:shadow-xl transition-all animate-bounce-slow'
+			>
+				<Image
+					src='/bot.svg'
+					alt='Chatbot'
+					width={50}
+					height={50}
+					className='w-12 h-12'
+				/>
+			</button>
+		</div>
 	);
 }
